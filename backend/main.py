8dc308f9a6b53b29f7f8c6003a248c3cb065a25e
terenkur <hillsboro96@gmail.com>
2025-07-17 01:22:40 +0300

    Use PORT env var for render

--- backend/main.py
+++ backend/main.py
@@ -158,4 +158,7 @@ async def wheel_settings_options():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
